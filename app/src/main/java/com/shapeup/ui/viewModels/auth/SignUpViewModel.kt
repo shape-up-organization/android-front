923c0d239a9b221b865642e302787d3712b6e5e8
VitorMendesco <vitormendesco.js@gmail.com>
2023-09-02 10:48:36 -0300

    feat(viewModels): adding signUp viewModel

---
+++ app/src/main/java/com/shapeup/ui/viewModels/auth/SignUpViewModel.kt
@@ -0,0 +1,71 @@
+package com.shapeup.ui.viewModels.auth
+
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+
+class SignUpViewModel : ViewModel() {
+    val activeStep = mutableIntStateOf(1)
+    val birthday = mutableStateOf("")
+    val email = mutableStateOf("")
+    val firstName = mutableStateOf("")
+    val lastName = mutableStateOf("")
+    val password = mutableStateOf("")
+    val passwordConfirmation = mutableStateOf("")
+    val phone = mutableStateOf("")
+    val username = mutableStateOf("")
+    val verificationCode = mutableStateOf("")
+
+    private fun clearFormData() {
+        activeStep.intValue = 1
+        birthday.value = ""
+        email.value = ""
+        firstName.value = ""
+        lastName.value = ""
+        password.value = ""
+        passwordConfirmation.value = ""
+        phone.value = ""
+        username.value = ""
+        verificationCode.value = ""
+    }
+
+    private fun signUp() {
+        println("activeStep: ${activeStep.intValue}")
+        println("birthday: ${birthday.value}")
+        println("email: ${email.value}")
+        println("firstName: ${firstName.value}")
+        println("lastName: ${lastName.value}")
+        println("password: ${password.value}")
+        println("passwordConfirmation: ${passwordConfirmation.value}")
+        println("phone: ${phone.value}")
+        println("username: ${username.value}")
+        println("verificationCode: ${verificationCode.value}")
+    }
+
+    private fun verifyCode() {}
+
+    val handlers = SignUpFormHandlers(
+        clearFormData = ::clearFormData,
+        signUp = ::signUp,
+        verifyCode = ::verifyCode
+    )
+}
+
+class SignUpFormData(
+    val birthday: MutableState<String>,
+    val email: MutableState<String>,
+    val firstName: MutableState<String>,
+    val lastName: MutableState<String>,
+    val password: MutableState<String>,
+    val passwordConfirmation: MutableState<String>,
+    val phone: MutableState<String>,
+    val username: MutableState<String>,
+    val verificationCode: MutableState<String>
+)
+
+class SignUpFormHandlers(
+    val clearFormData: () -> Unit,
+    val signUp: () -> Unit,
+    val verifyCode: () -> Unit
+)
