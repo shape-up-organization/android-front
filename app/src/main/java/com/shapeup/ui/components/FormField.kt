050aeb7213152215cdbcee7ab79ce6ab2f4148e1
VitorMendesco <vitormendesco.js@gmail.com>
2023-09-02 10:44:43 -0300

    feat(components): improving component and adding new types

--- app/src/main/java/com/shapeup/ui/components/FormField.kt
+++ app/src/main/java/com/shapeup/ui/components/FormField.kt
@@ -1,7 +1,10 @@
 package com.shapeup.ui.components
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -13,32 +16,41 @@ import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusManager
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
 import com.shapeup.ui.utils.constants.Icon
+import com.shapeup.ui.utils.helpers.PhoneNumberVisualTransformation
 
 @Composable
 fun FormField(
+    focusManager: FocusManager? = null,
     iconDescription: String = "",
     isPasswordHidden: MutableState<Boolean> = remember { mutableStateOf(true) },
+    keyboardActions: KeyboardActions = KeyboardActions(
+        onDone = { focusManager?.clearFocus() },
+        onSearch = { focusManager?.clearFocus() },
+        onSend = { focusManager?.clearFocus() }
+    ),
+    keyboardOptions: KeyboardOptions? = null,
     label: String,
+    maxLines: Int = 1,
+    @SuppressLint("ModifierParameter") modifier: Modifier = Modifier,
     onValueChange: (String) -> Unit = {},
+    readOnly: Boolean = false,
     supportingText: String = "",
     trailingIcon: Icon? = null,
     type: FormFieldType = FormFieldType.DEFAULT,
     value: String = ""
 ) {
+    val isDateType = type == FormFieldType.DATE
     val isPasswordType = type == FormFieldType.PASSWORD
-    val visualTransformation = when {
-        isPasswordType && isPasswordHidden.value -> {
-            PasswordVisualTransformation()
-        }
-
-        else -> VisualTransformation.None
-    }
+    val isPhoneType = type == FormFieldType.PHONE
 
     OutlinedTextField(
         colors = OutlinedTextFieldDefaults.colors(
@@ -49,14 +61,42 @@ fun FormField(
             unfocusedBorderColor = Color.Transparent,
             unfocusedContainerColor = MaterialTheme.colorScheme.primaryContainer
         ),
+        keyboardActions = keyboardActions,
+        keyboardOptions = when {
+            keyboardOptions != null -> keyboardOptions
+
+            isPasswordType -> KeyboardOptions(
+                imeAction = ImeAction.Done,
+                keyboardType = KeyboardType.Password
+            )
+
+            isPhoneType -> KeyboardOptions(
+                imeAction = ImeAction.Done,
+                keyboardType = KeyboardType.Phone
+            )
+
+            else -> KeyboardOptions(
+                imeAction = ImeAction.Next,
+                keyboardType = KeyboardType.Text
+            )
+        },
         label = { Text(label) },
         modifier = Modifier
-            .fillMaxWidth(),
+            .fillMaxWidth()
+            .then(modifier),
         onValueChange = onValueChange,
+        readOnly = readOnly,
         shape = RoundedCornerShape(16.dp),
         supportingText = { Text(supportingText) },
         trailingIcon = {
             when {
+                isDateType -> {
+                    Icon(
+                        contentDescription = "Calendar icon",
+                        painter = painterResource(id = Icon.Calendar.value)
+                    )
+                }
+
                 isPasswordType -> {
                     val icon = when {
                         (isPasswordHidden.value) -> Icon.EyeOpen
@@ -83,6 +123,13 @@ fun FormField(
                     }
                 }
 
+                isPhoneType -> {
+                    Icon(
+                        contentDescription = "Phone field",
+                        painter = painterResource(id = Icon.Phone.value)
+                    )
+                }
+
                 else -> {
                     trailingIcon?.let { icon ->
                         Icon(
@@ -94,11 +141,24 @@ fun FormField(
             }
         },
         value = value,
-        visualTransformation = visualTransformation
+        maxLines = maxLines,
+        visualTransformation = when {
+            isPasswordType && isPasswordHidden.value -> {
+                PasswordVisualTransformation()
+            }
+
+            isPhoneType -> {
+                PhoneNumberVisualTransformation()
+            }
+
+            else -> VisualTransformation.None
+        }
     )
 }
 
 enum class FormFieldType {
+    DATE,
     DEFAULT,
-    PASSWORD
+    PASSWORD,
+    PHONE
 }
--- app/src/main/java/com/shapeup/ui/utils/constants/Icon.kt
+++ app/src/main/java/com/shapeup/ui/utils/constants/Icon.kt
@@ -5,9 +5,12 @@ import com.shapeup.R
 
 enum class Icon(@DrawableRes val value: Int) {
     ArrowBack(value = R.drawable.icon_arrow_back),
+    Calendar(value = R.drawable.icon_calendar),
+    Close(value = R.drawable.icon_close),
     EyeClosed(value = R.drawable.icon_eye_closed),
     EyeOpen(value = R.drawable.icon_eye_open),
     FitnessCenter(value = R.drawable.icon_fitness_center),
     Groups(value = R.drawable.icon_groups),
+    Phone(value = R.drawable.icon_phone),
     SportsJoystick(value = R.drawable.icon_sports_joystick)
 }
