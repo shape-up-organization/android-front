2b5e5746243da23c1d32082f95808337f3528307
VitorMendesco <vitormendesco.js@gmail.com>
2023-09-02 21:21:39 -0300

    fix(SplashScreen): change background gradient to vertical direction

--- app/src/main/java/com/shapeup/ui/theme/ColorsBase.kt
+++ app/src/main/java/com/shapeup/ui/theme/ColorsBase.kt
@@ -12,23 +12,21 @@ val GreenDark = Color(0xFF23C7A8)
 val GreenDark50 = Color(0xFF10463C)
 val BackgroundDark = Color(0xFF2B2B2B)
 val BackgroundSecondaryDark = Color(0xFF181818)
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
 
 val Pink = Color(0xFFE96BE4)
 val BlackText = Color(0xFF2B2B2B)
 val WhiteText = Color(0xFFFFFFFF)
 
-val GradientLight = Brush.horizontalGradient(
+val GradientLight = Brush.verticalGradient(
     listOf(
-        BackgroundWhite,
-        GreenLight50
+        GreenLight50,
+        BackgroundWhite
     )
 )
 
-val GradientDark = Brush.horizontalGradient(
+val GradientDark = Brush.verticalGradient(
     listOf(
-        BackgroundDark,
-        GreenDark50
+        GreenDark50,
+        BackgroundDark
     )
 )
