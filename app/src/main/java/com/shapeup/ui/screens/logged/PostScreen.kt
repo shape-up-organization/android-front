734a4f2e48ca04b7e7feaef1cc6df3a3f76d233c
VitorMendesco <vitormendesco.js@gmail.com>
2023-09-17 12:15:26 -0300

    feat(screens): adding post screen

---
+++ app/src/main/java/com/shapeup/ui/screens/logged/PostScreen.kt
@@ -0,0 +1,39 @@
+package com.shapeup.ui.screens.logged
+
+import android.annotation.SuppressLint
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.shapeup.service.posts.getPostsMock
+import com.shapeup.service.users.getUserDataMock
+import com.shapeup.ui.components.CardPost
+import com.shapeup.ui.theme.ShapeUpTheme
+import com.shapeup.ui.utils.helpers.Navigator
+import com.shapeup.ui.viewModels.logged.Post
+import com.shapeup.ui.viewModels.logged.User
+
+@SuppressLint("UnrememberedMutableState")
+@Preview
+@Composable
+fun PostPreview() {
+    ShapeUpTheme {
+        PostScreen(
+            navigator = Navigator(),
+            postData = getPostsMock[0],
+            user = getUserDataMock
+        )
+    }
+}
+
+@Composable
+fun PostScreen(
+    navigator: Navigator,
+    postData: Post,
+    user: User
+) {
+    CardPost(
+        fullScreen = true,
+        postData = postData,
+        navigator = navigator,
+        user = user
+    )
+}
