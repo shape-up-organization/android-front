3dbcde67da6b8762d06195d1cf7c947b99b2b7be
VitorMendesco <vitormendesco.js@gmail.com>
2023-09-19 23:07:32 -0300

    feat(carousel): adding steppers

--- app/src/main/java/com/shapeup/ui/components/Carousel.kt
+++ app/src/main/java/com/shapeup/ui/components/Carousel.kt
@@ -2,13 +2,21 @@ package com.shapeup.ui.components
 
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalConfiguration
@@ -21,10 +29,7 @@ fun Carousel(data: List<String>) {
     val pagerState = rememberPagerState(initialPage = 0) { data.size }
     val maxHeight = (LocalConfiguration.current.screenHeightDp * 0.6).dp
 
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-    ) {
+    Box(modifier = Modifier.fillMaxSize()) {
         HorizontalPager(
             key = { data[it] },
             pageContent = { index ->
@@ -39,5 +44,26 @@ fun Carousel(data: List<String>) {
             },
             state = pagerState
         )
+
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(12.dp),
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .padding(bottom = 32.dp)
+        ) {
+            repeat(pagerState.pageCount) { index ->
+                Box(
+                    modifier = Modifier
+                        .background(
+                            color = when (index <= pagerState.currentPage) {
+                                true -> MaterialTheme.colorScheme.primary
+                                else -> MaterialTheme.colorScheme.tertiary
+                            },
+                            shape = RoundedCornerShape(16.dp)
+                        )
+                        .size(8.dp)
+                )
+            }
+        }
     }
 }
