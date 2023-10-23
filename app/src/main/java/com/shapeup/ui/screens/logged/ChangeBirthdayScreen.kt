b5065735d4e0408ca2382815d9cbe450135f7614
Andrey-Benicchio <andrey.bizerra@sptech.school>
2023-10-22 23:59:23 -0300

    :rocket: ajustes

--- app/src/main/java/com/shapeup/ui/screens/logged/ChangeBirthdayScreen.kt
+++ app/src/main/java/com/shapeup/ui/screens/logged/ChangeBirthdayScreen.kt
@@ -19,7 +19,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -45,7 +44,7 @@ fun ChangeBirthdayPreview() {
 fun ChangeBirthdayScreen(
     navigator: Navigator
 ) {
-    val focusManager = LocalFocusManager.current
+
 
     BackHandler {
         navigator.navigateBack()
@@ -92,14 +91,9 @@ fun ChangeBirthdayScreen(
 
                 DatePicker(
                     label = ("New birthday"),
-                    
-
-                )
 
 
-
-
-
+                    )
             }
         }
 
